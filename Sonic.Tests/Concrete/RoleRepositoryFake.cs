ef7f62db40addcc3ee249c349820c447c6545c63
teja-1010100 <tejastudio@gmail.com>
2016-11-20 00:29:47 +0100

    Add RoleRepository.

---
+++ Sonic.Tests/Concrete/RoleRepositoryFake.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Sonic.Domain.Abstract;
+using Sonic.Domain.Entities;
+using System.Linq;
+
+namespace Sonic.Tests.Concrete
+{
+    public class RoleRepositoryFake : ICrudRepository<Role>
+    {
+        private List<Role> storage = new List<Role>();
+
+        public IEnumerable<Role> GetAll()
+        {
+            return storage.AsEnumerable();
+        }
+
+        public Role GetById(int id)
+        {
+            return storage.FirstOrDefault(p => p.RoleId == id);
+        }
+
+        public bool Add(Role item)
+        {
+            storage.Add(item);
+
+            return true;
+        }
+                
+        public bool Update(Role item)
+        {
+            Role entity = storage.FirstOrDefault(p => p.RoleId == item.RoleId);
+            entity.Name = item.Name;
+            
+            return true;
+        }
+
+        public bool Remove(int id)
+        {
+            Role entity = storage.FirstOrDefault(p => p.RoleId == id);
+            storage.Remove(entity);
+
+            return true;
+        }
+    }
+}
---
+++ Sonic.Tests/Concrete/RoleRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using Sonic.Domain.Abstract;
+using Sonic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sonic.Tests.Concrete
+{    
+    public class RoleRepositoryTests
+    {
+        private readonly ICrudRepository<Role> repository = null;
+
+        public RoleRepositoryTests()
+        {
+            repository = new RoleRepositoryFake();
+        }
+
+        [Fact]
+        public void get_all()
+        {
+            var system = new Domain.Entities.System() { SystemId = 1, Name = "App 1" };
+            repository.Add(new Role() { RoleId = 1, SystemId = 1, Name = "App 1 - admin", System = system });
+            repository.Add(new Role() { RoleId = 2, SystemId = 1, Name = "App 1 - user", System = system });
+
+            Assert.Equal(2, repository.GetAll().Count());
+        }
+
+        [Fact]
+        public void get_by_id()
+        {
+            var system = new Domain.Entities.System() { SystemId = 1, Name = "App 1" };
+            repository.Add(new Role() { RoleId = 1, SystemId = 1, Name = "App 1 - admin", System = system });
+            repository.Add(new Role() { RoleId = 2, SystemId = 1, Name = "App 1 - user", System = system });
+
+            Assert.Equal(2, repository.GetAll().Count());
+            Assert.Equal("App 1 - user", repository.GetById(2).Name);
+        }
+
+        public void add()
+        {
+            var system = new Domain.Entities.System() { SystemId = 1, Name = "App 1" };
+            repository.Add(new Role() { RoleId = 1, SystemId = 1, Name = "App 1 - admin", System = system });
+            repository.Add(new Role() { RoleId = 2, SystemId = 1, Name = "App 1 - user", System = system });
+
+            Assert.Equal(2, repository.GetAll().Count());
+            Assert.Equal("App 1 - user", repository.GetById(2).Name);
+            Assert.Equal(1, repository.GetAll().Count());
+        }
+
+        [Fact]
+        public void update()
+        {
+            var system = new Domain.Entities.System() { SystemId = 1, Name = "App 1" };
+            repository.Add(new Role() { RoleId = 1, SystemId = 1, Name = "App 1 - admin", System = system });
+            repository.Add(new Role() { RoleId = 2, SystemId = 1, Name = "App 1 - user", System = system });
+
+            Role role = repository.GetById(2);
+            role.Name = "Test";
+            repository.Update(role);
+
+            Assert.Equal("Test", repository.GetById(2).Name);
+        }
+
+        [Fact]
+        public void remove()
+        {
+            var system = new Domain.Entities.System() { SystemId = 1, Name = "App 1" };
+            repository.Add(new Role() { RoleId = 1, SystemId = 1, Name = "App 1 - admin", System = system });
+            repository.Add(new Role() { RoleId = 2, SystemId = 1, Name = "App 1 - user", System = system });
+
+            repository.Remove(1);
+
+            Assert.Equal(1, repository.GetAll().Count());
+        }
+    }
+}
